06fd15feb2b4b025a134c5f5128dde27e93d7853
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-09-03 18:17:37 -0500

    refactor: move merging logic to #merge method definition, add more comprehensive test

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,27 +1,29 @@
-require 'pry'
-
 def merge_sort(array)
   if array.size < 2
     return array
   else
     left = merge_sort(array.slice!(0..(array.size / 2) - 1))
     right = merge_sort(array)
+    merge(left, right)
   end
-  merged = []
+end
+
+def merge(left, right, merged = [])
   until left.empty? && right.empty?
-    #binding.pry
     if left.empty?
       return merged + right
     elsif right.empty?
       return merged + left
     else
       left[0] < right[0] ? merged << left.shift : merged << right.shift
-      #binding.pry
     end
   end
   merged
 end
 
-p merge_sort([4, 3, 2, 1])
-p merge_sort([5, 8, 7, 1, 4, 9])
-p merge_sort([3, 1, 2])
+arr = []
+rand(100).times do
+  arr << rand(200)
+end
+
+p merge_sort(arr)
