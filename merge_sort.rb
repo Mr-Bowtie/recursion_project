d1b4f9384aa30cd657c13c66e8dd16f1c0a0b80e
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-09-03 18:26:38 -0500

    docs: add explanation of code for educational benefit

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,7 +1,12 @@
 def merge_sort(array)
+  # This is the base case
+  # single (or zero) element arrays are considered sorted
   if array.size < 2
     return array
   else
+    #This splits the input array in 2.
+    #The left side will be smaller if the array has an odd number of elements
+    #This is arbitrary
     left = merge_sort(array.slice!(0..(array.size / 2) - 1))
     right = merge_sort(array)
     merge(left, right)
@@ -10,11 +15,15 @@ end
 
 def merge(left, right, merged = [])
   until left.empty? && right.empty?
+    # if one side is empty, the other side is considered to be already sorted
+    # Returning the current holding array with the pre-sorted side decreases iterations
     if left.empty?
       return merged + right
     elsif right.empty?
       return merged + left
     else
+      # Compare the first elements from both halves
+      # push the lower of the two into the holding array
       left[0] < right[0] ? merged << left.shift : merged << right.shift
     end
   end
